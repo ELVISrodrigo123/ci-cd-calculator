40451412c39e5b665ff28945489b23ec9951e49b
ELVISrodrigo123 <ec7902812@gmail.com>
2025-08-30 07:40:24 -0400

    ci/cd deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -66,6 +66,7 @@ export default function Home() {
         <Display operation={operation} value={display} />
         <Keypad onPress={handlePress} />
         <h1>hola Emi</h1>
+        <h2>vamos a cantar el viernes verdad ¿?</h2>
       </div>
     </>
   );
