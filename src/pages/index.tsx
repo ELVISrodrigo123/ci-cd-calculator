ff26d5451f985c2a10134ae057af82a26501c325
ELVISrodrigo123 <ec7902812@gmail.com>
2025-08-30 08:45:12 -0400

    ci/cd deploy prueba

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -66,7 +66,7 @@ export default function Home() {
         <Display operation={operation} value={display} />
         <Keypad onPress={handlePress} />
         <h1>no puede ser </h1>
-        <h2>Buenos dias señores</h2>
+        <h2>Buenos dias criaturitas del mundo </h2>
       </div>
     </>
   );
